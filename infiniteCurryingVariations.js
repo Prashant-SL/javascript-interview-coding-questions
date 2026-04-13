206c6fabe1516a5c5285bc688d58538d1d2c3a85
Prashant-SL <prashant.laxmeshwarmath@gmail.com>
2026-04-13 18:56:47 +0530

    Currying basic version - 2 & 3

--- infiniteCurryingVariations.js
+++ infiniteCurryingVariations.js
@@ -1,12 +1,52 @@
 // Basic Version
-function sum(a) {
-    return function (b) {
-        if (!b) {
+// function sum(a) {
+//     return function (b) {
+//         if (!b) {
+//             return a;
+//         }
+
+//         return sum (a + b);
+//     }
+// }
+
+// console.log(sum(2)(3)(4)());
+
+// Version - 1
+// function sum(a) {
+//     return function inner(...args) {
+//         if (args.length == 0) {
+//             return a;
+//         }
+
+//         for (let arg of args) {
+//             a += arg;
+//         }
+
+//         return inner;
+//     }
+// }
+
+// console.log(sum(2)(3,4,5)(4)());
+
+// Version - 2
+function sum(...args) {
+    let a = 0;
+
+    for (let arg of args) {
+        a += arg;
+    }
+
+    return function inner(...args) {
+        if (args.length == 0) {
             return a;
         }
 
-        return sum (a + b);
+        for (let arg of args) {
+            a += arg;
+        }
+
+        return inner;
     }
 }
 
-console.log(sum(2)(3)(4)());
+console.log(sum(2,3,4)(3,4,5)(4)());
